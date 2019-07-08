70f319ffe2a7c3bb41538481437a86ca375e0654
kevin <kevin@kdoran.com>
2019-07-08 13:34:15 +0900

    Make the project its own Python package. Making imports and testing easier.

---
+++ README.md
@@ -0,0 +1,4 @@
+Install the src folder as a package root in order to run tests and get Pycharm
+hints.
+
+    pip install -e . 
---
+++ setup.py
@@ -0,0 +1,8 @@
+import setuptools
+
+setuptools.setup(
+    name='micronet',
+    package=['micronet'],
+    # This sets the directory, src, to be the root package (denoted by '').
+    package_dir={'': 'src'}
+)
