a5b5d4b2637fa1f156ed045aff3224d76b6b98fa
kevin <kevin@kdoran.com>
2019-07-24 17:41:43 +0900

    Download MobileNetv2 checkpoint. Ignore download location.

---
+++ scripts/download_mobilenetv2_checkpoint.sh
@@ -0,0 +1,3 @@
+wget -P ./resources 'https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_224.tgz'
+tar -xzf ./resources/mobilenet_v2_1.0_224.tgz -C ./resources
+rm ./resources/mobilenet_v2_1.0_224.tgz
