f2f263a4509b47f9adcbbd902295db249516ebb9
kevin <kevin@kdoran.com>
2019-07-28 11:27:09 +0900

    Added script for downloading official MobileNetv2 checkpiont.

--- scripts/download_mobilenetv2_checkpoint.sh
+++ scripts/download_mobilenetv2_checkpoint.sh
@@ -1,3 +1,5 @@
-wget -P ./resources 'https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_224.tgz'
-tar -xzf ./resources/mobilenet_v2_1.0_224.tgz -C ./resources
-rm ./resources/mobilenet_v2_1.0_224.tgz
+wget 'https://storage.googleapis.com/mobilenet_v2/checkpoints/mobilenet_v2_1.0_224.tgz'
+mkdir './resources/mobilenetv2_checkpoints/'
+mkdir './resources/mobilenetv2_checkpoints/1.0_224/'
+tar -xzf ./mobilenet_v2_1.0_224.tgz -C './resources/mobilenetv2_checkpoints/1.0_224/'
+rm ./mobilenet_v2_1.0_224.tgz
